a0934e148545ec548eba5ab26a6c58bdf5e9e306
Sebastian Michaelsen <sebastian@michaelsen.io>
2020-09-04 07:20:41 +0200

    Set BRANCH in .env and pass it to the ci-build-script

--- trigger_build.sh
+++ trigger_build.sh
@@ -4,13 +4,13 @@ source .env
 rm -rf workspace
 
 # checkout project in workspace
-git clone --depth 1 $REPOSITORY_URL workspace
+git clone -b $BRANCH --depth 1 $REPOSITORY_URL workspace
 
 # copy ci build script into workspace
 cp ci-build-script.sh workspace/
 
 # start worker
 docker run -d -v $PWD/workspace:/workspace --name benkins_worker -w /workspace $IMAGE
-docker exec -ti benkins_worker sh ci-build-script.sh
+docker exec -e "BRANCH=${BRANCH}" -ti benkins_worker sh ci-build-script.sh
 docker stop benkins_worker
 docker rm benkins_worker
