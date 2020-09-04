6f4088d59eecf5402065536eafd7690c384eb02d
Sebastian Michaelsen <sebastian@michaelsen.io>
2020-09-04 08:02:40 +0200

    Pass build workspace from outside to trigger_build.sh

--- src/trigger_build.sh
+++ src/trigger_build.sh
@@ -1,16 +1,20 @@
-echo "Building ${BRANCH} branch of ${REPOSITORY_URL} with image ${IMAGE}"
+echo "Building ${BRANCH} branch of ${REPOSITORY_URL} with image ${IMAGE}.\n"
+echo "Local workspace path: ${LOCAL_WORKSPACE}"
 
-# clear workspace
-rm -rf workspace
+# clear workspace if it is safe
+case $LOCAL_WORKSPACE/ in
+  $PWD/*) echo "Clearing workspace"; rm -rf $LOCAL_WORKSPACE;;
+  *) echo "Local workspace is not safe to delete! Aborting."; exit 200;;
+esac
 
 # checkout project in workspace
-git clone -b $BRANCH --depth 1 $REPOSITORY_URL workspace
+git clone -b $BRANCH --depth 1 $REPOSITORY_URL $LOCAL_WORKSPACE
 
 # copy ci build script into workspace
-cp ci-build-script.sh workspace/
+cp ci-build-script.sh ${LOCAL_WORKSPACE}/
 
 # start worker
-docker run -d -v $PWD/workspace:/workspace --name benkins_worker -w /workspace $IMAGE
+docker run -d -v ${LOCAL_WORKSPACE}:/workspace --name benkins_worker -w /workspace $IMAGE
 docker exec -e "BRANCH=${BRANCH}" -ti benkins_worker sh ci-build-script.sh
 docker stop benkins_worker
 docker rm benkins_worker
