ebceee0a029b84407458af8d2060f51353ed6f5d
Sebastian Michaelsen <sebastian@michaelsen.io>
2020-10-02 13:06:17 +0200

    [FEATURE] Provide benkins access keys in build container

--- src/trigger_build.sh
+++ src/trigger_build.sh
@@ -2,11 +2,13 @@
 
 echo "\nBuilding ${BRANCH} with image ${IMAGE}."
 
-# copy ci build script into workspace
-cp ci-build-script.sh ${LOCAL_WORKSPACE}/
+# copy bekins resources into workspace
+mkdir -p ${LOCAL_WORKSPACE}/benkins
+cp ci-build-script.sh ${LOCAL_WORKSPACE}/benkins/
+cp keys/key ${LOCAL_WORKSPACE}/benkins/
 
 # start worker
 docker run -d -v ${LOCAL_WORKSPACE}:/workspace --name benkins_worker -w /workspace $IMAGE
-docker exec -e "BRANCH=${BRANCH}" -ti benkins_worker sh ci-build-script.sh
+docker exec -e "BRANCH=${BRANCH}" -ti benkins_worker sh benkins/ci-build-script.sh
 docker stop benkins_worker
 docker rm benkins_worker
